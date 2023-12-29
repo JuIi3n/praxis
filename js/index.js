1023f6ab4ff8212e4367aae1820572af5ae226bf
JuIi3n <julian.kappesser@outlook.de>
2023-12-29 15:38:45 +0100

    s

--- js/index.js
+++
@@ -1,7 +0,0 @@
-const menu_toggle = document.getElementById("menu_toggle")
-const menu = document.getElementById("menu_container")
-
-menu_toggle.onclick = function() {
-  menu_toggle.classList.toggle("nav-active")
-  setTimeout(() => {menu.classList.toggle("nav-active")}, 800)
-}
