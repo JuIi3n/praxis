dcbf5c125e1719b51ec287857ad3105379eee0f0
JuIi3n <69055693+JuIi3n@users.noreply.github.com>
2023-12-29 15:05:45 +0100

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header")
 
 window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
+  const currentScroll = window.scrollY;
   if (currentScroll > 50) {
     header.dataset.position = "ontop";
   } else {
