9480c3d29921a0098e142ddca7186299adc83210
JuIi3n <julian.kappesser@outlook.de>
2023-12-29 18:22:55 +0100

    s3

--- js/aktuelles.js
+++ js/aktuelles.js
@@ -1,4 +1,4 @@
-const main = document.querySelector("main");
+const main = document.querySelector(".main");
 
 
 
@@ -9,21 +9,30 @@ async function loadData() {
       throw new Error('Bad response')
     }
     const data = await json_file.json()
-    data.forEach((element, i) => {
+    main.innerHTML = ""
+    if(data.length == 0) {
       let item = '<section class="container flex">'
-      + `<h1>${element.title}</h1>`
-      + `<h2>${element.subtitle}</h2>`
-      + `<p>${element.information}</p>`
+      + `<h1>Keine Neuigkeiten</h1>`
+      + `<p>Aktuell gibt es keine Neuigkeiten</p>`
       + '</section>'
-
-      if(i > 0) {
-        item = '<hr>' + item
-      }
-
       main.innerHTML += item
       delete item
-    })
-    
+    }else {
+      data.forEach((element, i) => {
+        let item = '<section class="container flex">'
+        + `<h1>${element.title.replaceAll('\n', '<br>')}</h1>`
+        + `<h2>${element.subtitle.replaceAll('\n', '<br>')}</h2>`
+        + `<p>${element.information.replaceAll('\n', '<br>')}</p>`
+        + '</section>'
+
+        if(i > 0) {
+          item = '<hr>' + item
+        }
+
+        main.innerHTML += item
+        delete item
+      })
+    }
   }catch(e) {
     alert("Beim Laden der Seite ist ein Fehler aufgetreten. Die Seite wird neu geladen.")
     location.reload()
